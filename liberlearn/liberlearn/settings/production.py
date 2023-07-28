4590b612b9f733cb8ce892ec8ba3efc416cc98f2
Atunde Goodness <atundeg@gmail.com>
2023-07-28 20:11:58 +0100

    configure allowed hosts and csrf

--- liberlearn/liberlearn/settings/production.py
+++ liberlearn/liberlearn/settings/production.py
@@ -1,3 +1,4 @@
 from .base import *
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["web-production-3640.up.railway.app", "127.0.0.1"]
+CSRF_TRUSTED_ORIGINS = ["https://web-production-3640.up.railway.app"]
